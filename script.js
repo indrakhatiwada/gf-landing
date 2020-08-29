8714d5e66c21534b9aab3f177940eb104951466e
indrakhatiwada <khatiwadai024@gmail.com>
2020-08-30 00:56:03 +0545

    fixing the relative path issue is github pages

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function imgSlider(param) {
 
 function changeBg(param) {
   const sec = document.querySelector(".section");
-  const text = document.querySelector(".order");
+
   sec.style.background = param;
-  text.style.color = param + "!important";
 }
