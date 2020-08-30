5038a6f1962989bb7e229602d2385e30ca934246
indrakhatiwada <khatiwadai024@gmail.com>
2020-08-30 08:03:28 +0545

    more correction for github pages

--- script.js
+++
@@ -1,9 +0,0 @@
-function imgSlider(param) {
-  document.querySelector(".gamefuel").src = param;
-}
-
-function changeBg(param) {
-  const sec = document.querySelector(".section");
-
-  sec.style.background = param;
-}
